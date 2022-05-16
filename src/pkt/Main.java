ac0ce2c7f1faee05b700a600a6159db494546809
miguel <miguel@miguel>
2022-05-16 09:13:26 +0200

    Commit

---
+++ src/pkt/Main.java
@@ -0,0 +1,15 @@
+package pkt;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Buenos días ceinmark, Soy Miguel Veléz");
+		System.out.println("Introduce un numero para averiguar si es primo o no");
+		int numero = sc.nextInt();
+	}
+
+}
