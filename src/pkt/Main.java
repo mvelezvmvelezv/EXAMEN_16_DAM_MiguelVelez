e67aa199f66e74bfa165e58cca5f86d1516910d2
miguel <miguel@miguel>
2022-05-16 09:35:20 +0200

    Commit Rama1

--- src/pkt/Main.java
+++ src/pkt/Main.java
@@ -9,7 +9,28 @@ public class Main {
 		
 		System.out.println("Buenos días ceinmark, Soy Miguel Veléz");
 		System.out.println("Introduce un numero para averiguar si es primo o no");
-		int numero = sc.nextInt();
+		int num = sc.nextInt();
+		
+		String primo  = "";
+		String divisores = "";
+		
+		if (num == 0 || num == 1) {
+			System.out.println("El numero no es primo y no tienen divisores");
+		}else {
+		
+			for (int i = 2; i < num; i++) {
+				
+				if (num % i == 0) {
+					primo = "El numero no es primo";
+					divisores += i + "/";
+				}else {
+					primo = "El numero es primo";
+				}
+			}
+			
+			System.out.println(primo);
+			System.out.println("Sus divisores son: " + divisores);
+		}
 	}
 
 }
